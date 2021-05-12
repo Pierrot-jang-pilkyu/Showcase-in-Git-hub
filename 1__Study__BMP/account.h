2a25d86a0a4f4a17662eb69b40635f4d356ccf03
Pierrot-Jang-pilkyu <jangpilkyu1@gmail.com>
2021-05-12 23:13:13 +0900

    May. 12. 2021
    main.cpp source code is changed to Object-Oreinted thinking.
    
    1. One step
     : class account -> account.h
    
    Korean Hangul
     main.cpp 소스 코드를 객체지향적 코드로 나눴다.
    
     1. 첫 단계 : 클래스 -> 헤더파일

--- 1__Study__BMP/account.h
+++ 1__Study__BMP/account.h
@@ -1,17 +1,16 @@
-// Bank Account Manage Program
+// Account management class
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
 
 #include <iostream>
 #include <string>
-#include <random> 
 #include <chrono>
-#include <vector>
-#include <utility> 
+#include <random>
 #include <limits>
 
 using namespace std;
-// program user =  manager's ID
-static string ManagerID = "pb_bank123";
-static string ManagerPW = "pb_bank321";
+
 
 class Account{
 private :
@@ -64,130 +63,12 @@ public :
 	}
 	
 };
+
 // scan integer value ignoring '\n'.
 int getInt(); 
-// task processing UI by number.
-void taskNum(int *num, int nowCustomers); 
-// add account toi dynamic memory allocation
-// void addMemAccount(Account *nowAccount, int nowCustomers);
-// find account by name ( retouch this function )
-int accountByName(Account *nowAccount, int nowCustomers);  
 
-bool managerLogin();
-// print account of all customers 
-void allShowBalance(Account *nowAccount, int nowCustomers);
+// Function is implemented. Under coding
 
-int main(){
-	static int allCustomers = 0;
-	static Account *allAccount = new Account;
-	
-	string choice; // in task processing choice variable
-	int num = 0; // in task UI number initialize 
-	int Amount = 0; // deposit or withdraw Amount initialize
-	int index = -1; // return value in compare all list and input name. 
-	
-	while(1){
-		
-		taskNum(&num, allCustomers);
-		
-		
-		
-		switch(num){
-			case 1 : // add new account
-				 
-				allCustomers++;
-				
-				if(allCustomers == 1){
-					allAccount[allCustomers - 1].newCustomer();
-				}
-				else if(allCustomers == 2){
-					Account *temp = new Account;
-					temp->accountCopy(allAccount[0]);
-					delete allAccount;
-					allAccount = new Account[allCustomers];
-					allAccount[0].accountCopy(temp[0]);
-					allAccount[allCustomers - 1].newCustomer();
-					delete temp;
-				}
-				else{
-					Account *temp = new Account[allCustomers];
-					for(int i = 0; i < allCustomers-1; i++){
-						temp[i].accountCopy(allAccount[i]);
-					}
-					delete []allAccount;
-					allAccount = new Account[allCustomers];
-					for(int i = 0; i < allCustomers-1; i++){
-						allAccount[i].accountCopy(temp[i]);
-					}
-					allAccount[allCustomers - 1].newCustomer();
-					delete []temp;
-				}
-				break;
-			case 2 : // deposit
-				index = accountByName(allAccount, allCustomers);
-				if(allCustomers == index){
-					// no matched 
-					cout << "Do not invalid value. Return to start menu." << endl;
-					break; 
-				}
-				allAccount[index].deposit(&Amount);
-				break;
-			case 3 : // withdraw
-				index = accountByName(allAccount, allCustomers);
-				if(allCustomers == index){
-					// no matched
-					cout << "Do not invalid value. Please restart." << endl;
-					break; 
-				}
-				allAccount[index].withdraw(&Amount);
-				break;
-			case 4 : // Balance inquiry
-				index = accountByName(allAccount, allCustomers);
-				if(allCustomers == index){
-					cout << "Do not invalid value. Please restart." << endl;
-					break; 
-				}
-				allAccount[index].showBalance();
-				break;
-			case 0 : // manager-only menu
-				if(managerLogin()){
-					int mNum;
-					cout << "All customers balance inquiry. Press no.0 : "; mNum = getInt();
-					cout << endl << endl << endl;
-					allShowBalance(allAccount, allCustomers);
-					break;
-				}
-				else{
-					cout << "Login failed." << endl;
-					cout << "Please restart." << endl;
-				}
-				break;
-			default :
-				cout << "Do not invalid value." << endl;
-				cout << "Please check and press number again." << endl;
-				break;
-		}
-		
-		
-		cout << "Return to start menu? (Y / N) : ";
-		getline(cin, choice);
-		if(choice == "y" || choice == "Y" || choice == "ㅛ"){
-			continue;
-		}
-		else if(choice == "n" || choice == "N" || choice == "ㅜ"){
-			break;
-		}
-		else{
-			cout << "Wrong input data. Return to start menu. " << endl;
-		}
-		
-	}
-	
-	
-	delete []allAccount;
-	
-	return 0;
-}
 
 void Account::newCustomer(){
 	this->newAccount();
@@ -341,7 +222,8 @@ void Account::deposit(int *inAmount){
 		
 		cout << "Deposit amount : "; *inAmount = getInt();
 		
-		if(*inAmount > (numeric_limits<int>::max() / 10)){
+		// int max : 214748364
+		if(*inAmount > (numeric_limits<int>::max()) / 2){
 			cout << "The amount is too large to be processed." << endl;
 			cout << "Talk to the person in charge." << endl << endl;
 			break;
@@ -371,9 +253,10 @@ void Account::withdraw(int *outAmount){
 		if(*outAmount > 0){
 			if(this->balance == 0){
 				cout << "There is no account balance." << endl;
+				break;
 			}
 			else if(this->balance < *outAmount){
-				cout << "You account balance is insufficient" << endl;
+				cout << "Your account balance is insufficient" << endl;
 			}
 			else{
 				this->balance -= *outAmount;
@@ -417,6 +300,8 @@ void Account::accountCopy(Account act){
 	return;
 }
 
+// A temporary measure
+
 int getInt(){
 	int in;
 	
@@ -441,104 +326,8 @@ int getInt(){
 		}
 	}
 }
-	
-void taskNum(int *num, int nowCustomers){
-	
-	cout << "Currently, the number of accounts on the sever is : " << nowCustomers;
-	cout << endl << endl << endl; 
-	
-	cout << "Please enter the task number to be processed." << endl << endl;
-	cout << "Make new account : Enter no.1" << endl;
-	cout << "Deposit : Enter no.2" << endl;
-	cout << "Withdraw : Enter no.3, " << endl;
-	cout << "Balance iquiry : Enter no.4" << endl;
-	cout << "Manager-only menu : Enter no.0" << endl << endl << endl;
-	cout << "Entered by number : ";
-	
-	*num = getInt();
-	
-	return;
-	
-}
-/* error -> Implemented in main().	
-void addMemAccount(Account *nowAccount, int nowCustomers){
-	int i;
-	Account *tempAccount = new Account[nowCustomers];
-	
-	for(i = 0; i < nowCustomers-1; i++){
-		cout << i + 3 << endl;
-		tempAccount[i].accountCopy(nowAccount[i]);
-	}
-	delete []nowAccount;
-	
-	nowAccount = new Account[nowCustomers];
-	
-	for(i=0; i< nowCustomers; i++){
-		cout << i + 8 << endl;
-		nowAccount[i].accountCopy(tempAccount[i]);
-	}
-	
-	delete []tempAccount;
-}*/
-
-int accountByName(Account *nowAccount, int nowCustomers){
-	int i; string name;
-	
-	cout << "Enter the customer's name : "; getline(cin, name);
-
-	for(i = 0; i < nowCustomers; i++){
-		if(nowAccount[i].compareName(name) == 0){
-			nowAccount[i].showAccount();
-			break;
-		}
-	}
-	
-	if(i == nowCustomers){
-		char c;
-		cout << "There is no account to that customer." << endl;
-	}
-	return i;
-}
-
-bool managerLogin(){
-	int compareID =2, comparePW = 2;
-	string ID, PW;
-	
-	cout << "Manager I	D : "; getline(cin, ID);
-	compareID = ManagerID.compare(ID);
-	if(compareID == 0){
-		cout << "Manager PASSWORD : "; getline(cin, PW);
-		comparePW = ManagerPW.compare(PW);
-		
-		if(comparePW == 0){
-			cout << "Lonined." << endl;
-			return true;
-		}
-		else{
-			cout << "Wrong Manager PASSWORD." << endl;
-		}
-	}
-	else{
-		cout << "Wrong Manager ID." << endl; 
-	}
-	
-	return false;
-}
-
-void allShowBalance(Account *nowAccount, int nowCustomers){
-	int i;
-	
-	for(i = 0; i < nowCustomers; i++){
-		nowAccount[i].showName();
-		nowAccount[i].showAccount();
-		nowAccount[i].showBalance();
-		cout << endl << endl; 
-	}
-	return;
-}
 
 
 
 
-
-
+#endif
